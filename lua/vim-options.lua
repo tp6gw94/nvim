e3a6b3b0121a9e2957a43b920a00caaf48491712
todd <tp6gw94@gmail.com>
2024-07-15 22:00:37 +0800

    tmux

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,10 +7,10 @@ vim.cmd("set relativenumber")
 vim.cmd("set ignorecase smartcase")
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- vim.keymap.set("n", "<C-j>", "<C-w>j")
+-- vim.keymap.set("n", "<C-k>", "<C-w>k")
+-- vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<esc>", ":nohlsearch<CR>", {
 	remap = true,
 })
