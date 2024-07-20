b66799d37e4e53b47d5ccc6c7ada50e22a06fb66
todd <tp6gw94@gmail.com>
2024-07-20 09:49:15 +0800

    bashls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "dockerls", "graphql", "tsserver", "jsonls" },
+        ensure_installed = { "lua_ls", "tsserver", "jsonls", "bashls" },
       })
     end,
   },
@@ -47,6 +47,10 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
 
       lspconfig.jsonls.setup({
         capabilities = capabilities,
